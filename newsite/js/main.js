c81d4ff4c7cbe3d5ca5267411be143fd8a363f0b
stevencampos15 <stevencampos15@gmail.com>
2023-04-14 09:06:50 -0600

    undo menu

--- newsite/js/main.js
+++ newsite/js/main.js
@@ -41,21 +41,6 @@
    $.Scrollax();
 
 
-   //Menu
-   function updateMenuButton() {
-	$('.js-menu-button').find('.menu-icon').toggleClass('is-active');
-}
-
-$(document).ready(function() {
-
-	$('.js-menu-button').click(function(e){
-
-		e.preventDefault();
-		updateMenuButton();
-
-	});
-
-});
 
    // Burger Menu
 	var burgerMenu = function() {
